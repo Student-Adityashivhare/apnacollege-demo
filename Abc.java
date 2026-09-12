2230a3a7a0317d3d83a640a06e182b91426acf20
student Adityashivhare <adityashivhare7984@gmail.com>
2026-09-13 01:24:19 +0530

    add new file

---
+++ Abc.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Abc{
+    public static void main(String[] args) {
+        Scanner s=new Scanner (System.in);
+        System.out.println("Enter your name :");
+        String name = s.next();
+        System.out.println("Enter your age:");
+        int age = s.nextInt();
+        System.out.println("name:" +name);
+        System.out.println("age:" +age);
+    }
+}
