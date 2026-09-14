9b200d03900073d4c07f97b31cb8dbd65bb604eb
student Adityashivhare <adityashivhare7984@gmail.com>
2026-09-14 23:03:35 +0530

    inherit

--- Abc.java
+++ Abc.java
@@ -1,12 +1,18 @@
 import java.util.Scanner;
-class Abc{
+
+class Abc {
     public static void main(String[] args) {
-        Scanner s=new Scanner (System.in);
+        Scanner s = new Scanner(System.in);
+
         System.out.println("Enter your name :");
-        String name = s.next();
+        String name = s.nextLine();
+
         System.out.println("Enter your age:");
         int age = s.nextInt();
-        System.out.println("name:" +name);
-        System.out.println("age:" +age);
+
+        System.out.println("name:" + name);
+        System.out.println("age:" + age);
+
+        s.close();
     }
 }
