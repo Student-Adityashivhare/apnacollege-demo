6ee5d46419a4b5e700ff4bf6b07f2ee9024191e0
student Adityashivhare <adityashivhare7984@gmail.com>
2026-09-19 00:25:50 +0530

    add some in c

---
+++ Demo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int num;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    if(num%2 ==0){
+        printf("enen nuber");
+    }
+    else{
+        printf("odd number");
+    }
+
+}
